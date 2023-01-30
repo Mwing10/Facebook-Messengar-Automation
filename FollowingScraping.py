26cfde465606a528074e05ee53ac023a78a5bcee
THEpWn3R-o <pwn3er889@gmail.com>
2023-01-30 08:32:46 -0500

    rough followers function

--- FollowingScraping.py
+++ FollowingScraping.py
@@ -40,6 +40,13 @@ time.sleep(5)
 
 # open the facebook page
 driver.get("https://www.facebook.com/ExtonRegionChamber/following")
+def get_following():
+    following = []
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    for link in soup.find_all('', class_=''): # <== when not on school wifi add class names for soup
+        following.append(link.get('href')) # think about checking this up.. dont look right
+    return following
+
 time.sleep(10)
 
 
