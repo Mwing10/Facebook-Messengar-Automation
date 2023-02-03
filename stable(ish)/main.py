bff4175fcadbb939563b98bb37091e1ae663bf28
PWN3R offical <pwn3er889@gmail.com>
2023-02-02 22:00:52 -0500

    optimization

--- stable(ish)/main.py
+++ stable(ish)/main.py
@@ -3,9 +3,7 @@ try:
     from selenium.webdriver.common.keys import Keys
     from webdriver_manager.chrome import ChromeDriverManager
     from selenium.webdriver.chrome.service import Service as ChromeService
-    from bs4 import BeautifulSoup
-    import time, random, os, csv, subprocess
-    import pandas as pd
+    import time, os, csv, subprocess
     from alive_progress import alive_bar
 except ImportError as e:
     print("Unable to import some libraries, please install them first with 'python3 -m pip install -r requirements.txt'\n", e)
